364837b4b11316255fd5f7923152db7b85eec7a4
ai_len <is.qinxu@gmail.com>
2024-11-15 15:02:14 +0800

    初始化仓库代码文件

---
+++ Cpp_code/util.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cstring>
+#include<cassert>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<thread>
+#include<mutex>
+#include<sstream>
+#include<ranges>
+#include<memory>
+using namespace std;
